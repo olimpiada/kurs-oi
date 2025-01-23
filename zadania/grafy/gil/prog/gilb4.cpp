e0d7b90002a0832e77524faff68ac75df2045552
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-01-23 15:21:02 +0100

    zadania: gildie

---
+++ zadania/grafy/gil/prog/gilb4.cpp
@@ -0,0 +1,12 @@
+/*
+ * Weryfikujące rozwiązanie błędne do zadania Gildie/mpil005
+ * Autor: Juliusz Sompolski
+ * Data: 17.09.2009
+ * Zawsze odpowiada NIE, żeby sprawdzić czy taki testy są zgrupowane.
+ */
+#include <cstdio>
+
+int main() {
+	printf("NIE\n");
+	return 0;
+}
